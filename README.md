91522c087ef1ad2c8585cb6fe2ab3918630298dc
carlosperate <carlosperate@users.noreply.github.com>
2019-07-26 01:30:25 +0100

    Add README and update setup.py.

---
+++ README.md
@@ -0,0 +1,16 @@
+# MkDocs Awesome List Plugin
+
+MkDocs Plugin to inject social media cards for each entry in an awesome-list. 
+
+To use this plugin install it with pip in the same environment than MkDocs:
+
+```
+pip install MkDocsAwesomeListPlugin
+```
+
+Then add the following entry in the Mkdocs config file:
+
+```yml
+plugins:
+  - awesome-list
+```
--- setup.py
+++ setup.py
@@ -1,19 +1,32 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup
 
-setup(
-    name='MkDocsAwesomeListPlugin',
-    version='0.1.0',
-    packages=['mkdocs_awesome_list_plugin'],
-    url='https://github.com/carlosperate/mkdocs-awesome-list-plugin',
-    license='MIT',
-    author='Carlos Pereira Atencio',
-    author_email='carlosperate@embeddedlog.com',
-    description='Description.',
-    install_requires=['mkdocs'],
+with open("README.md") as f:
+    readme = f.read()
 
-    # The following rows are important to register your plugin.
-    # The format is "(plugin name) = (plugin folder):(class name)"
-    # Without them, mkdocs will not be able to recognize it.
+setup(
+    name="MkDocsAwesomeListPlugin",
+    version="0.1.0",
+    description="MkDocs Plugin to inject social media cards for each entry in an awesome-list.",
+    long_description=readme,
+    keywords=["mkdocs", "plugin", "awesome", "list"],
+    author="Carlos Pereira Atencio",
+    author_email="carlosperate@embeddedlog.com",
+    url="https://github.com/carlosperate/mkdocs-awesome-list-plugin",
+    license="MIT license",
+    packages=["mkdocs_awesome_list_plugin"],
+    install_requires=["mkdocs", "webpreview>=1.6.0"],
+    python_requires=">=3.4, <4",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    # This entry point is necessary for MkDocs to be able to use the plugin
     entry_points={
         'mkdocs.plugins': [
             'awesome-list = mkdocs_awesome_list_plugin.awesomelist:AwesomeList',
